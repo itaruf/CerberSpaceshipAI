dba0d8dfcc8b570551df613a9dc9812a8d7b34c5
Hugo <hugo.tuil@edu.devinci.fr>
2021-10-13 11:00:37 +0200

    Move To Wp to fix

--- Assets/Teams/ExampleTeam/CerberController.cs
+++ Assets/Teams/ExampleTeam/CerberController.cs
@@ -14,7 +14,12 @@ namespace TeamCerber
         private bool needShoot;
         private bool needMine;
         private bool needShockwave;
+        private bool followPlayer;
+        private bool moveToWp;
 
+        private Vector2 closestWpPosition;
+
+        float targetOrient;
         public override void Initialize(SpaceShipView spaceship, GameData data)
         {
             behaviorTree = GetComponent<BehaviorTree>();
@@ -27,11 +32,29 @@ namespace TeamCerber
             needShoot = (bool)behaviorTree.GetVariable("Need Shoot").GetValue();
             needMine = (bool)behaviorTree.GetVariable("Need Mine").GetValue();
             needShockwave = (bool)behaviorTree.GetVariable("Need Shockwave").GetValue();
+            followPlayer = (bool)behaviorTree.GetVariable("Follow Player").GetValue();
+            moveToWp = (bool)behaviorTree.GetVariable("Move to Wp").GetValue();
 
             SpaceShipView otherSpaceship = data.GetSpaceShipForOwner(1 - spaceship.Owner);
+
             float thrust = 1.0f;
+
             //float targetOrient = spaceship.Orientation + 90.0f;
-            float targetOrient = -Mathf.Atan2(otherSpaceship.Position.x - spaceship.Position.x, otherSpaceship.Position.y - spaceship.Position.y) * Mathf.Rad2Deg + 90;
+
+            foreach (WayPointView WPChild in data.WayPoints)
+            {
+                if (Vector2.Distance(closestWpPosition, spaceship.Position) > Vector2.Distance(WPChild.Position, spaceship.Position) && WPChild.Owner != spaceship.Owner)
+                {
+                    closestWpPosition = WPChild.Position;
+                }
+            }
+
+            if (followPlayer)
+                targetOrient = -Mathf.Atan2(otherSpaceship.Position.x - spaceship.Position.x, otherSpaceship.Position.y - spaceship.Position.y) * Mathf.Rad2Deg + 90;
+            else if (moveToWp)
+                targetOrient = -Mathf.Atan2(closestWpPosition.y - spaceship.Position.x, closestWpPosition.y - spaceship.Position.y) * Mathf.Rad2Deg + 90;
+            else if (!followPlayer && !moveToWp)
+                targetOrient = spaceship.Orientation;
 
 
             bool canHit = AimingHelpers.CanHit(spaceship, otherSpaceship.Position, otherSpaceship.Velocity, 0.15f);
--- Assets/Teams/ExampleTeam/MoveToWP.cs
+++ Assets/Teams/ExampleTeam/MoveToWP.cs
@@ -9,30 +9,34 @@ namespace BehaviorDesigner.Runtime.Tasks.Unity.UnityCharacterController
 {
     public class MoveToWP : Action
     {
-        public SharedGameObject _spaceShip;
-        private SpaceShip spaceShip;
-        public BehaviorTree behaviorTree;
-        private SharedVector2 closestWpPosition;
+        //public SharedGameObject _spaceShip;
+        //private SpaceShip spaceShip;
+        //public BehaviorTree behaviorTree;
+        //public SharedVector2 closestWpPosition;
+
+        public SharedBool goToWaypoint;
 
         public override void OnAwake()
         {
-            behaviorTree = GetComponent<BehaviorTree>();
-            spaceShip = _spaceShip.Value.gameObject.GetComponent<SpaceShip>();
+            //behaviorTree = GetComponent<BehaviorTree>();
+            //spaceShip = _spaceShip.Value.gameObject.GetComponent<SpaceShip>();
             base.OnAwake();
         }
 
         public override TaskStatus OnUpdate()
         {
-            closestWpPosition = (SharedVector2) behaviorTree.GetVariable("Closest Wp Position");
+            goToWaypoint.Value = true;
+
+            //closestWpPosition = (SharedVector2) behaviorTree.GetVariable("Closest Wp Position");
             //Debug.Log(closestWpPosition.Value);
 
             // On fait bouger le vaisseau vers le wp le plus proche
-            if (Vector2.SqrMagnitude(spaceShip.Position - closestWpPosition.Value) < 0.1f)
-            {
-                return TaskStatus.Success;
-            }
+            //if (Vector2.SqrMagnitude(spaceShip.Position - closestWpPosition.Value) < 0.1f)
+            //{
+            //    return TaskStatus.Success;
+            //}
    
-            return TaskStatus.Running;
+            return base.OnUpdate();
         }
 
         public override void OnEnd()
